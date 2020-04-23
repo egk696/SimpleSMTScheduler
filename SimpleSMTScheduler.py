9724fdb916b8b20d929d42145c2ca08f51d3438a
Eleftherios Kyriakakis <elky@dtu.dk>
2020-04-23 15:32:16 +0200

    Updated scheduling constraints now generate full activation instances for the hyper-period and consider per-task allowed jitter and relative offsets and deadlines

--- SimpleSMTScheduler.py
+++ SimpleSMTScheduler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
+# Author: Eleftherios Kyriakakis
 
 import csv
-# Author: Eleftherios Kyriakakis (egk696@hotmail.com)
 import getopt
 from math import *
 from time import *
@@ -24,32 +24,40 @@ interactive = False
 
 
 class Task:
-    def __init__(self, period: float, deadline: float, execution: float, name: str, fixed_pit: int = None,
-                 later_pit: int = None, cfunc: str = "void"):
+
+    def __init__(self, period: float, execution: float, deadline: float, offset: int, jitter: int,
+                 name: str, fixed_pit: int = None, cfunc: str = "void"):
         self.name = name
         self.period = ceil(period)
         self.deadline = ceil(deadline)
         self.execution = ceil(execution)
-        self.start_pit = Int(name + "_pit")
+        self.offset = offset
+        self.jitter = ceil(jitter)
+        self.release_instances = []
         self.cfunc = cfunc
+        self.activation_instances = []
+
         if fixed_pit is not None:
             self.fixed_pit = fixed_pit
 
-        if later_pit is not None:
-            self.later_pit = later_pit
-
-        print("--Task_" + name + "()")
-        print("\tT=%s μs" % period)
-        print("\tD=%s μs" % deadline)
-        print("\tC=%s μs" % execution)
+        print("Task_" + name + " {")
+        print("  T=%s," % period)
+        print("  C=%s," % execution)
+        print("  D=%s," % deadline)
+        print("  O=%s," % offset)
+        print("  J=%s," % jitter)
+        print("};")
         if fixed_pit is not None:
             print("\tS=%s μs" % fixed_pit)
 
-    def setStartPIT(self, pit: float):
-        self.pit = pit
+    def addStartPIT(self, pit: float):
+        self.activation_instances.append(pit)
+
+    def setActivationInstancePIT(self, i: int, activation_pit: float):
+        self.activation_instances[i] = activation_pit
 
     def getStartPIT(self):
-        return self.pit
+        return self.activation_instances
 
 
 def find_lcm(numbers):
@@ -59,69 +67,80 @@ def find_lcm(numbers):
     return lcm
 
 
-def gen_schedule(task_set, allocated_jitter):
+def z3_abs(x):
+    return If(x >= 0, x, -x)
+
+
+def gen_schedule(task_set, wcet_gap):
     # Test utilization
-    utilization = sum(t.execution / t.period for t in taskSet)
-    print("\nUtilization = %s" % utilization)
-    if utilization > len(taskSet) * (2 ** (1 / len(taskSet)) - 1):
-        print("\nSufficient schedulability test failed")
+    utilization = sum(t.execution / t.period for t in taskSet) * 100
+    print("\nUtilization = %s %%" % utilization)
     # Find the hyper period
     hyper_period = find_lcm([o.period for o in task_set])
-    print("\nSchedule hyper period = %s" % hyper_period)
+    print("Schedule hyper period = %s" % hyper_period)
     # Define constraints
-    s = Solver()
-    # Search for distinct values
-    s.add(Distinct([t.start_pit for t in task_set]))
-    # The sum of durations should not exceed the hyper period
-    s.add(Sum([t.start_pit + t.execution for t in task_set]) <= hyper_period)
+    smt = Solver()
+    for test_task in taskSet:
+        for nn in range(floor(hyper_period / test_task.period)):
+            test_task.release_instances.append(Int(test_task.name + "_" + "inst_" + str(nn)))
     # Search for task specific
-    for task in task_set:
-        # Each task should finish within the deadline
-        # s.add(task.start_pit + task.execution < task.deadline)
-        # If a task has a user fixed start PIT define it
-        if hasattr(task, 'fixed_pit'):
-            s.add(task.start_pit == task.fixed_pit)
-        elif hasattr(task, 'later_pit'):
-            s.add(task.start_pit >= task.later_pit)
-        else:
-            s.add(task.start_pit >= 0)
-        # Constraints against the other tasks
-        others = [o for o in task_set if o != task]
-        for nn in range(floor(hyper_period / task.period)):
-            s.add(nn * task.period + task.start_pit + task.execution < nn * task.period + task.deadline)
-            for ot in others:
-                # The start PIT should not fall within the execution of another task including a BAG
-                for kk in range(floor(hyper_period / task.period)):
-                    s.add(Or(
-                        nn * task.period + task.start_pit + task.execution + allocated_jitter < kk * ot.period + ot.start_pit,
-                        nn * task.period + task.start_pit > kk * ot.period + ot.start_pit + ot.execution + allocated_jitter))
-
+    for test_task in task_set:
+        for nn in range(len(test_task.release_instances)):
+            smt.add(test_task.release_instances[nn] + test_task.execution <= hyper_period)
+            # If a task has a user fixed start PIT define it
+            if hasattr(test_task, 'fixed_pit'):
+                smt.add(test_task.release_instances[nn] == nn * test_task.period + test_task.fixed_pit)
+            else:
+                smt.add(test_task.release_instances[nn] >= nn * test_task.period + test_task.offset)
+            # Period constraint including jitter
+            if nn > 0:
+                smt.add(And(
+                    test_task.release_instances[nn] - test_task.release_instances[
+                        nn - 1] >= test_task.period - test_task.jitter,
+                    test_task.release_instances[nn] - test_task.release_instances[
+                        nn - 1] <= test_task.period + test_task.jitter
+                )
+                )
+            # Each task should finish within the deadline with jitter
+            smt.add(test_task.release_instances[
+                        nn] + test_task.execution <= nn * test_task.period + test_task.deadline + test_task.jitter)
+            # The start PIT should not fall within the execution of another task including a BAG
+            for other_task in [o for o in task_set if o != test_task]:
+                for kk in range(len(other_task.release_instances)):
+                    smt.add(Or(
+                        test_task.release_instances[nn] + test_task.execution + wcet_gap <=
+                        other_task.release_instances[kk],
+                        test_task.release_instances[nn] >= other_task.release_instances[
+                            kk] + other_task.execution + wcet_gap
+                    )
+                    )
     # Try to solve
     elapsed_time = 0
     start_time = time()
-    if s.check() == sat:
-        m = s.model()
+    if smt.check() in (sat, unknown):
+        solution_model = smt.model()
         elapsed_time = time() - start_time
-        print("\nSatisfied by the following activation times:")
-        print(m)
     else:
-        m = None
-        print("\nCould not be solved")
+        solution_model = None
+        time() - start_time
+        print("\nCould not be solved: ", str(smt.check()))
 
     if verbose:
+        print("\nModel Solution:")
+        print(solution_model)
         print("\nAsserted constraints...")
-        for c in s.assertions():
+        for c in smt.assertions():
             print(c)
         print("\nZ3 statistics...")
-        for k, v in s.statistics():
+        for k, v in smt.statistics():
             print("%s : %s" % (k, v))
 
-        print("\nSolver completed in %s ms" % (elapsed_time * SEC_TO_MS))
+    print("\nSolver completed in %s ms\n" % (elapsed_time * SEC_TO_MS))
 
-    return m, hyper_period
+    return solution_model, hyper_period
 
 
-def plot_schedule(task_set, hyper_period, periods):
+def plot_schedule(task_set, hyper_period, iterations):
     # Declaring a figure "gnt"
     fig, axis = plt.subplots()
 
@@ -129,17 +148,18 @@ def plot_schedule(task_set, hyper_period, periods):
     axis.set_ylim(0, len(task_set) * 10)
 
     # Setting X-axis limits
-    axis.set_xlim(0, periods * Sum([t.getStartPIT() + t.execution for t in task_set]))
-    # axis.set_xlim(0, hyperPeriod * periods)
+    axis.set_xlim(0, hyperPeriod * iterations)
+
     # Setting labels for x-axis and y-axis
     axis.set_xlabel('Schedule Timeline')
     axis.set_ylabel('Tasks')
 
     # axis.set_xticks(range(0, periods*Sum([t.getStartPIT()+t.execution for t in taskSet]), 5000))
-    axis.set_xticks(range(0, hyper_period * periods, int(hyper_period * periods / 10)))
+    axis.set_xticks(range(0, hyper_period * iterations, int(hyper_period * iterations / 10)))
 
     # Setting ticks on y-axis
     axis.set_yticks(range(5, len(task_set) * 10 + 5, 10))
+
     # Labelling tickes of y-axis
     axis.set_yticklabels([t.name for t in task_set])
 
@@ -153,19 +173,14 @@ def plot_schedule(task_set, hyper_period, periods):
     # Color map
     cmap = plt.cm.get_cmap('jet', len(task_set))
 
-    print("\nSchedule printout for %s periods" % periods)
+    print("Schedule plotted for %s hyper-periods\n" % iterations)
 
     for i in range(len(task_set)):
-        print(taskSet[i].name, end='')
-        print("[", end='')
-        # Constructing task execution
         taskExecutionBars = []
-        for jj in range(floor(hyper_period * periods / task_set[i].period)):
-            taskExecutionBars.append((jj * task_set[i].period + task_set[i].getStartPIT(), task_set[i].execution))
-            print(jj * taskSet[i].period + taskSet[i].getStartPIT(), end='')
-            print(",", end='')
-        print("]\n")
-        # Declaring a bar in schedule
+        for hyper_iter in range(0, iterations):
+            for jj in range(len(task_set[i].getStartPIT())):
+                taskExecutionBars.append(
+                    (hyper_iter * hyper_period + task_set[i].getStartPIT()[jj], task_set[i].execution))
         axis.broken_barh(taskExecutionBars, (i * 10, 10), label=task_set[i].name, linewidth=0.3, edgecolors='black',
                          facecolors=cmap(i))
 
@@ -176,29 +191,30 @@ def plot_schedule(task_set, hyper_period, periods):
 
 
 if __name__ == "__main__":
-    print("\nWelcome to this simple SMT scheduler (SSMTS)...")
+    print("Welcome to this simple SMT scheduler (SSMTS)...\n")
 
     if len(sys.argv) > 1:
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hi:o:j:n:pvc",
-                                       ["help", "itasks=", "osched=", "jitter=", "nperiods=", "plot", "verbose",
+            opts, args = getopt.getopt(sys.argv[1:], "hi:o:w:n:pvc",
+                                       ["help", "itasks=", "osched=", "wcet=", "nperiods=", "plot", "verbose",
                                         "code"])
         except getopt.GetoptError:
-            print('Try : SimpleSMTScheduler.py -i <inputtasks> -o <outputschedule> -j 35713 -n <plotperiods> -p -v')
+            print('Try : SimpleSMTScheduler.py -i <inputtasks> -o <outputschedule> -w 35713 -n <plotperiods> -p -v')
             print("Or : SimpleSMTScheduler.py --help")
             sys.exit(2)
         plot = False
         verbose = False
         code = False
         schedulePlotPeriods = 1
+        wcet_offset = 0
         jitter = 0
         for opt, arg in opts:
             if opt in ("-h", "--help"):
-                print('SimpleSMTScheduler.py -i <inputtasks> -o <outputschedule> -j 35713 -n <plotperiods> -p -v')
+                print('SimpleSMTScheduler.py -i <inputtasks> -o <outputschedule> -w 35713 -n <plotperiods> -p -v')
                 print("-h\t--help")
                 print("-i\t--itasks")
                 print("-o\t--osched")
-                print("-j\t--jitter")
+                print("-w\t--wcet")
                 print("-n\t--nperiods")
                 print("-p\t--plot")
                 print("-v\t--verbose")
@@ -208,8 +224,8 @@ if __name__ == "__main__":
                 tasksFileName = arg
             elif opt in ("-o", "--osched"):
                 scheduleFileName = arg
-            elif opt in ("-j", "--jitter"):
-                jitter = int(arg)
+            elif opt in ("-w", "--wcet"):
+                wcet_offset = int(arg)
             elif opt in ("-n", "--nperiods"):
                 schedulePlotPeriods = int(arg)
             elif opt in ("-p", "--plot"):
@@ -221,16 +237,17 @@ if __name__ == "__main__":
     else:
         code = False
         interactive = True
+        wcet_offset = 0
         jitter = 0
 
     if interactive:
-        tasksFileName = input("\nEnter the csv file for the tasks to be scheduled: ")
-        jitter = int(input("Enter the jitter allocation to be accounted for: "))
+        tasksFileName = input("Enter the csv file for the tasks to be scheduled: ")
+        wcet_offset = int(input("Enter the WCET allocation gap between activations: "))
         schedulePlotPeriods = int(input("Enter the number of hyper periods to be plotted: "))
         verbose = input("Enable statistics (Yes/No)? ") == "Yes"
-        print("\n")
 
     with open(tasksFileName, 'r') as f:
+        print("Importing task set from file source...\n")
         reader = csv.reader(f)
         isFirstRow = True
         rowIndex = 0
@@ -243,42 +260,47 @@ if __name__ == "__main__":
                 except ValueError:
                     period = 0
                 try:
-                    deadline = float(row[1])
-                except ValueError:
-                    deadline = 0
-                try:
-                    execution = float(row[2])
+                    execution = float(row[1])
                 except ValueError:
                     execution = 0
                 try:
-                    name = str(row[3])
+                    deadline = float(row[2])
                 except ValueError:
-                    name = "Task %s" % rowIndex
+                    deadline = 0
                 try:
-                    fixed = float(row[4])
+                    offset = float(row[3])
+                except ValueError:
+                    offset = 0
+                try:
+                    jitter = float(row[4])
+                except ValueError:
+                    jitter = 0
+                try:
+                    fixed = float(row[5])
                 except ValueError:
                     fixed = None
                 try:
-                    later = float(row[5])
+                    name = str(row[6]).strip()
                 except ValueError:
-                    later = None
+                    name = "Task %s" % rowIndex
                 try:
-                    func = str(row[6])
+                    func = str(row[7])
                 except ValueError:
                     func = "void"
 
-                taskSet.append(Task(period, deadline, execution, name, fixed, later, func))
+                taskSet.append(Task(period, execution, deadline, offset, jitter, name, fixed, func))
                 rowIndex = rowIndex + 1
 
     if [t for t in taskSet if t.execution > t.deadline]:
         sys.exit("\nTask set is not valid.\nExecution times violate period and deadline constraints")
     else:
-        schedule, hyperPeriod = gen_schedule(taskSet, jitter)
+        schedule, hyperPeriod = gen_schedule(taskSet, wcet_offset)
         if schedule is not None:
-            for i in range(len(taskSet)):
-                taskSet[i].setStartPIT(schedule.evaluate(taskSet[i].start_pit).as_long())
+            for task in taskSet:
+                for pit in task.release_instances:
+                    task.addStartPIT(schedule[pit].as_long())
 
-            taskSet.sort(key=lambda x: x.getStartPIT())
+            taskSet.sort(key=lambda x: x.getStartPIT()[0])
 
             if interactive:
                 schedulePlot = plot_schedule(taskSet, hyperPeriod, schedulePlotPeriods)
@@ -287,10 +309,9 @@ if __name__ == "__main__":
                 schedulePlot = plot_schedule(taskSet, hyperPeriod, schedulePlotPeriods)
                 schedulePlot.savefig(scheduleFileName, dpi=MY_DPI)
 
-            if code:
-                for i in range(len(taskSet)):
-                    print("init_minimal_tttask(&taskSet[%s], %s, %s, %s);" % (
-                        i, taskSet[i].period, taskSet[i].getStartPIT(), taskSet[i].cfunc))
+            print("Name\tActivation Instances\t")
+            for i in range(len(taskSet)):
+                print("%s\t%s" % (taskSet[i].name, taskSet[i].getStartPIT()))
 
             sys.exit()
         else:
